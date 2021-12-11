441577e2540940996fda02d927c508467d68b434
caroline parra gomes <Cargomes@server01.foursys.local>
2021-12-11 15:08:16 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[]args){
+
+        // crie um programa declarando uma variável do tipo string chamada nome, outra chamada celular e outra chamada cidade.
+        // atribua um valor para todas as variáveis e exiba o resultado no console;
+
+        String nome;
+        double celular;
+        String cidade;
+
+        System.out.println("caroline parra gomes");
+        System.out.println("11998087812");
+        System.out.println("Santana de parnaiba");
+
+    }
+}
